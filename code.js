d755a0d356d1cd55a67a0a3241eeb66bcbc10c88
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-02 13:49:29 -0600

    Initial commit for block 19 workshop. Psuedocode and linked .js

---
+++ code.js
@@ -0,0 +1,7 @@
+//A visitor enters the website to find a list of available freelancers;
+//Each freelancer has a Name, Occupation, and Starting Price for services;
+//The visitor observes "Alice", the writer, with a starting price of $30;
+//The visitor observes "Bob", the teacher, with a starting price of $50;
+//The visitor finds a message displaying the average price of the list of freelancers;
+
+console.log("Hello!")
