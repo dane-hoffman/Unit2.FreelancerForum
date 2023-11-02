c96e2ed6efabf4819063022e5907d2f882771ff3
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-02 15:27:10 -0600

    Calculated total price of all freelancers, and divided the by the total number of freelancers within the array to provide average price.

--- code.js
+++ code.js
@@ -2,6 +2,12 @@
 //Each freelancer has a Name, Occupation, and Starting Price for services;
 //The visitor observes "Alice", the writer, with a starting price of $30;
 //The visitor observes "Bob", the teacher, with a starting price of $50;
+
+//Create variables/elements
+const body = document.body;
+const h1Element = document.createElement("h1");
+const infoElement = document.createElement("section");
+const ulElement = document.createElement("ul");
 const freelancers = [
     { name: "Alice", price: 25, occupation: "gardener"},
     { name: "Dr. Pressure", price: 51, occupation: "programmer" },
@@ -12,37 +18,50 @@ const freelancers = [
     { name: "Dr. Wire", price: 47, occupation: "teacher" },
     { name: "Prof. Goose", price: 72, occupation: "driver" },
   ];
-  
-  const mapFreelancers = freelancers.map(({ name, occupation, price }) => ({
+  const mappedFreelancers = freelancers.map(({ name, occupation, price }) => ({
     Name: name,
     Occupation: occupation,
     Price: price,
   }));
-
-//The visitor finds a message displaying the average price of the list of freelancers;
-
-//Create variables/elements
-const body = document.body;
-const h1Element = document.createElement("h1");
-const infoElement = document.createElement("section");
-const ulElement = document.createElement("ul");
 
 //const flexContainer = document.createElement("section");
 
 //Add content
 h1Element.textContent = "Freelance Services";
 
+
 //Append variables/elements
 body.appendChild(h1Element);
 body.appendChild(infoElement);
 infoElement.appendChild(ulElement);
 
+//Calculate the average price of all freelancers
+const totalPrice = mappedFreelancers.reduce((sum, freelancer) => {
+    return sum + freelancer.Price;
+  }, 0);
+  
+  const numberOfFreelancers = mappedFreelancers.length;
+  const averagePrice = totalPrice / numberOfFreelancers;
+  console.log(averagePrice);
+// // Calculate the average price of all freelancers
+// const totalPrices = formattedFreelancers.reduce((sum, freelancer) => sum + freelancer.Price, 0);
+// const averagePrice = totalPrices / formattedFreelancers.length;
+
+// // Display the average price
+// const averagePriceElement = document.createElement("p");
+// averagePriceElement.textContent = `Average Price: $${averagePrice.toFixed(2)}`;
+
+// // Append the average price element to the section
+// infoSection.appendChild(averagePriceElement);
+
 // Iterate through the formattedFreelancers array and add them to the ul
-mapFreelancers.forEach(freelancer => {
+mappedFreelancers.forEach(freelancer => {
     const liElement = document.createElement("li");
-    liElement.textContent = `Name: ${freelancer.Name}, Occupation: ${freelancer.Occupation}, Price: $${freelancer.Price}`;
+    liElement.textContent = `Name: ${freelancer.name}, Occupation: ${freelancer.Occupation}, Price: $${freelancer.Price}`;
     ulElement.appendChild(liElement);
-  });
+});
+
+//The visitor finds a message displaying the average price of the list of freelancers;
 
 
 //Set attributes/'style' elements
