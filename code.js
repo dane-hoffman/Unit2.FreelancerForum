5bb72cd931fca25043e78de08d55a321ae2e0be3
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-02 14:24:26 -0600

    Appended the  to the .

--- code.js
+++ code.js
@@ -1,4 +1,24 @@
 //A visitor enters the website to find a list of available freelancers;
+const freelancers = [
+    { name: "Alice", price: 25, occupation: "gardener"},
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+  ];
+  
+//   const formattedFreelancers = freelancers.map(({ name, occupation, price }) => ({
+//     Name: name,
+//     Occupation: occupation,
+//     Price: price,
+//   }));
+  
+//   console.log(formattedFreelancers);
+
+  
 //Each freelancer has a Name, Occupation, and Starting Price for services;
 //The visitor observes "Alice", the writer, with a starting price of $30;
 //The visitor observes "Bob", the teacher, with a starting price of $50;
@@ -7,12 +27,18 @@
 //Create variables/elements
 const body = document.body;
 const h1Element = document.createElement("h1");
+const infoElement = document.createElement("section");
+const ulElement = document.createElement("ul");
+
+//const flexContainer = document.createElement("section");
 
 //Add content
 h1Element.textContent = "Freelance Services";
 
 //Append variables/elements
 body.appendChild(h1Element);
+body.appendChild(infoElement);
+infoElement.appendChild(ulElement);
 
 
 //Set attributes/'style' elements
