54eeae14b68146eadc802688c45969a17db15cd5
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-02 14:01:30 -0600

    Successfully added h1 element w/content using .js

--- code.js
+++ code.js
@@ -4,4 +4,15 @@
 //The visitor observes "Bob", the teacher, with a starting price of $50;
 //The visitor finds a message displaying the average price of the list of freelancers;
 
-console.log("Hello!")
+//Create variables/elements
+const body = document.body;
+const h1Element = document.createElement("h1");
+
+//Add content
+h1Element.textContent = "Freelance Services";
+
+//Append variables/elements
+body.appendChild(h1Element);
+
+
+//Set attributes/'style' elements
